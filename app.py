0335747903a5335240a198f7655c2103561f13e5
Zachary Snyder <sadin@fedoraproject.org>
2015-08-23 00:01:48 -0400

    Hooked up template

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, abort
 import redis
-app = Flask(__name__)
+app = Flask(__name__, static_path='/static')
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def index():
+    return render_template('index.html', )
 
 if __name__ == "__main__":
     app.run()
