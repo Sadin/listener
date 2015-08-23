bea0217fe08e42dacf07fc0229b2bb6ac4f8cc85
Zachary Snyder <sadin@fedoraproject.org>
2015-08-23 00:15:26 -0400

    redis test

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template, request, redirect, url_for, abort
 import redis
 app = Flask(__name__, static_path='/static')
 
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+r.set('foo', 'bar')
+redis_response = r.get('foo').decode('utf-8')
+
 @app.route('/')
 def index():
-    return render_template('index.html', )
 
-if __name__ == "__main__":
-    app.run()
+    return render_template('index.html', payload=redis_response )
+
+if __name__ == '__main__':
+    app.run(debug=True)
